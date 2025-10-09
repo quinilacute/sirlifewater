fa602236539275ba69bd67ee7aeefbfbc1e55d60
Peace Stephen Isuaiko <quinilacute2000@gmail.com>
2025-10-08 18:01:36 -0700

    Fix double router issue and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
@@ -13,23 +13,21 @@ import FAQ from "./pages/AboutFAQ";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white text-gray-800">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/cart" element={<Checkout />} />
-          <Route path="/order-confirmation" element={<OrderConfirmation />} />
-          <Route path="/login" element={<LoginSignup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/delivery" element={<DeliveryMap />} />
-          <Route path="/faq" element={<FAQ />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-white text-gray-800">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-confirmation" element={<OrderConfirmation />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/delivery" element={<DeliveryMap />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </div>
   );
 }
 
